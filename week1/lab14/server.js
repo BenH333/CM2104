c262b2c1a4d4aef47b07d9f4c6e13095351f2337
BenH333 <r.hadden@rgu.ac.uk>
2018-03-29 14:21:26 +0100

    Lab 14 - Finished

--- week1/lab14/server.js
+++ week1/lab14/server.js
@@ -4,22 +4,30 @@ var Twitter = require('twitter');
 var params = {screen_name:'nodejs'};
 
 var client = new Twitter({
-  consumer_key: 'xxxxxxxx',
-  consumer_secret:'xxxxxxx',
-  access_token_key:'xxxxxxx',
-  access_token_secret:'xxxxxxxx'
+  consumer_key: 'vPuzIzncL5b5HyyUMp99kbMwv',
+  consumer_secret: 'yKbWuC6TupPNzIJP0NnNXVxLn1D8BDprijakencUG3bAGMize5',
+  access_token_key: '14812487-PNs6GC8wpgeAG2W7szRt4ZvpTA7lYBes6mORd9KDT',
+  access_token_secret: 'Tjy5TrQ5c5YVLA6q2cb7LeoDty7bHIg0HmbDELgMwMCHG'
 });
 
  app.use(express.static('public'))
 
-  app.get('/',function(req, res){
-    client.get('statuses/user_timeline', params,function(error, tweets, response){
-      if(!error){
-        //console.log(tweets);
-        res.send(tweets);
-      }
-    });
-    res.send("Hello world! by express");
-  });
+ app.get('/tweetsjson', function(req, res) {
+   var params = {
+     screen_name: 'nodejs'
+   };
+   client.get('statuses/user_timeline', params, function(error, tweets, response) {
+     if (!error) {
+       var json = [];
+       for (var i = 0; i < tweets.length; i++) {
+         json.push({
+           name: tweets[i].user.name,
+           text: tweets[i].text
+         });
+       }
+       res.send(JSON.stringify(json));
+     }
+   });
+ });
 
-  app.listen(8080)
+ app.listen(8080);
