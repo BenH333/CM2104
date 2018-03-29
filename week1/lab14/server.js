7f1344e858c54fb82b6b65a995208f0a21c7bf87
BenH333 <r.hadden@rgu.ac.uk>
2018-03-29 14:03:12 +0100

    Lab 14 tweets api

--- week1/lab14/server.js
+++ week1/lab14/server.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
+var Twitter = require('twitter');
+var params = {screen_name:'nodejs'};
+
+var client = new Twitter({
+  consumer_key: 'xxxxxxxx',
+  consumer_secret:'xxxxxxx',
+  access_token_key:'xxxxxxx',
+  access_token_secret:'xxxxxxxx'
+});
 
  app.use(express.static('public'))
 
   app.get('/',function(req, res){
+    client.get('statuses/user_timeline', params,function(error, tweets, response){
+      if(!error){
+        //console.log(tweets);
+        res.send(tweets);
+      }
+    });
     res.send("Hello world! by express");
   });
 
