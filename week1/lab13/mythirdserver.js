768a448ee3a916444eeca8ffed65de646f52873d
BenH333 <r.hadden@rgu.ac.uk>
2018-04-09 10:55:04 +0100

    lab changes

--- week1/lab13/mythirdserver.js
+++ week1/lab13/mythirdserver.js
@@ -1,26 +1,8 @@
-/*var http = require('http');
-var knockknock = require('knock-knock-jokes');
+var http = require('http');
+var oneLinerJoke = require('one-liner-joke');
 
-
-  /*
-
-  /*
-  The variable getRandomJoke will contain a random joke with a format:
-  {"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
-  */
-  var getRandomJoke = oneLinerJoke.getRandomJoke();
-  var oneLinerJoke = require('one-liner-joke');
-  var getRandomJokeWithTag = oneLinerJoke.getRandomJokeWithTag('stupid');
-
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var randomJoke = oneLinerJoke()
-    res.end(randomJoke);
-    }).listen(8080);
-
-
-
-  /*
-  The variable getRandomJoke will contain a random joke with a tag and with a format:
-  {"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
-  */
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var randomJoke = oneLinerJoke()
+  res.end(randomJoke);
+  }).listen(8080);
