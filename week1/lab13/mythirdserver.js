415e444b1958db97b689f74c5ce45d04a46f943a
BenH333 <r.hadden@rgu.ac.uk>
2018-04-09 10:44:04 +0100

    Lab13 joke change

--- week1/lab13/mythirdserver.js
+++ week1/lab13/mythirdserver.js
@@ -1,8 +1,26 @@
-var http = require('http');
+/*var http = require('http');
 var knockknock = require('knock-knock-jokes');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var randomJoke = knockknock()
-  res.end(randomJoke);
-  }).listen(8080);
+
+  /*
+
+  /*
+  The variable getRandomJoke will contain a random joke with a format:
+  {"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
+  */
+  var getRandomJoke = oneLinerJoke.getRandomJoke();
+  var oneLinerJoke = require('one-liner-joke');
+  var getRandomJokeWithTag = oneLinerJoke.getRandomJokeWithTag('stupid');
+
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var randomJoke = oneLinerJoke()
+    res.end(randomJoke);
+    }).listen(8080);
+
+
+
+  /*
+  The variable getRandomJoke will contain a random joke with a tag and with a format:
+  {"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
+  */
