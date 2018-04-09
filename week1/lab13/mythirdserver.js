6c29f3b08979c4dbfeae2a7da735e074e0e781fc
BenH333 <r.hadden@rgu.ac.uk>
2018-04-09 11:04:47 +0100

    123

--- week1/lab13/mythirdserver.js
+++ week1/lab13/mythirdserver.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var joke = require('one-liner-joke');
+var randomJoke = joke.getRandomJoke();
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  var randomJoke = joke.getRandomJoke();
   res.end(randomJoke);
   }).listen(8080);
