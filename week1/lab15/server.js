733d0b75fae6cd9eac9001e3f2cc4a3167bf59ed
BenH333 <r.hadden@rgu.ac.uk>
2018-04-09 15:54:16 +0100

    lab 15

--- week1/lab15/server.js
+++ week1/lab15/server.js
@@ -1,9 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/star_wars_quotes";
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 var db;
 
@@ -24,6 +26,30 @@ app.get('/all', function(req, res) {
       output += "<h3>" + result[i].name + "</h3>"
       output += "<p>" + result[i].quote + "</p>"
       output += "</div>"
+    }
+    res.send(output);
+  });
+});
+
+app.post('/quotes', function (req, res) {
+    db.collection('quotes').save(req.body, function(err, result) {
+      if (err) throw err;
+      console.log('saved to database');
+      res.redirect('/');
+  });
+});
+
+app.post('/search', function(req, res) {
+  db.collection('quotes').find(req.body).toArray(function(err, result) {
+    if (err) throw err;
+
+    var output ="<h1>All the quotes</h1>";
+
+    for (var i =0; i < result.length; i++) {
+      output +="<div>"
+      output +="<h3>"+ result[i].name +"</h3>"
+      output +="<p>"+ result[i].quote +"</p>"
+      output +="</div>"
     }
     res.send(output);
   });
